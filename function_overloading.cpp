d6b635ed28d4ad4aac204cfea5ce51a5358e0a25
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-07-29 22:20:17 +0600

    change

--- function_overloading.cpp
+++ function_overloading.cpp
@@ -15,3 +15,24 @@ int main() {
 	test(10);
 	test(10.6);
 }
+
+
+//In The Name of ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+class sum {
+public:
+	void add (int a, int b) {
+		cout << a + b << "\n";
+	}
+	void add (int a, int b, int c) {
+		cout << a + b + c << "\n";
+	}
+};
+int main() {
+	sum obj;
+	obj.add(5, 10);
+	obj.add(5, 10, 15);
+}
+
+
--- function_overriding.cpp
+++ function_overriding.cpp
@@ -16,7 +16,39 @@ public:
 int main() {
 	child obj1;
 	obj1.test(5);
-	obj1.parent :: test(10);
+	//obj1.parent :: test(10);
+
+   return 0;
+}
+
+//In The Name of ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+class person {
+public: 
+	void display() {
+		cout << "I am a person\n";
+	}
+};
+class student : public person {
+public:
+	void display() {
+		cout << "I am a student\n";
+	}
+};
+class teacher : public person {
+public:
+	void display() {
+	cout << "I am a teacher\n";
+  }
+};
+int main() {
+	teacher t;
+	person p;
+	student s;
+	t.display();
+	p.display();
+	s.display();
 
    return 0;
 }
