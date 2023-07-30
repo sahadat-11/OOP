8787346950adae6e7ce0d5440f62217da5bd01c1
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-07-30 15:00:35 +0600

    changed

--- copy_constructor.cpp
+++ copy_constructor.cpp
@@ -1,80 +1,80 @@
-// // //In The Name of ALLAH
-// // #include<bits/stdc++.h>
-// // using namespace std;
-// // class myclass {
-// //    int a;
-// // public:
-// //     myclass() {
-// //         a = 10;
-// //     }
-// //     myclass(int n) {
-// //         a = n;
-// //     }
-// //     void display() {
-// //         cout << "The value is : " << a << "\n";
-// //     }
-// // };
-// // int main() {
-// //   myclass x, y, z(45);
-// //   x.display();
-// //   y.display();
-// //   z.display();
-// //   return 0;
-// // }
+//In The Name of ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+class myclass {
+   int a;
+public:
+    myclass() {
+        a = 10;
+    }
+    myclass(int n) {
+        a = n;
+    }
+    void display() {
+        cout << "The value is : " << a << "\n";
+    }
+};
+int main() {
+  myclass x, y, z(45);
+  x.display();
+  y.display();
+  z.display();
+  return 0;
+}
 
-// //In The Name of ALLAH
-// #include<bits/stdc++.h>
-// using namespace std;
-// class myclass {
-//    int a;
-// public:
-//     myclass() {
-//         a = 10;
-//     }
-//     myclass(int n) {
-//         a = n;
-//     }
-//     myclass(myclass & obj) {
-//         cout << "Copy Constructor Called\n";
-//         a = obj.a;
-//     }
-//     void display() {
-//         cout << "The value is : " << a << "\n";
-//     }
-// };
-// int main() {
-//   myclass x, y, z(45);
-//   x.display();
-//   y.display();
-//   z.display();
-//   myclass z1(z);
-//   z1.display();
-//   return 0;
-// }
+//In The Name of ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+class myclass {
+   int a;
+public:
+    myclass() {
+        a = 10;
+    }
+    myclass(int n) {
+        a = n;
+    }
+    myclass(myclass & obj) {
+        cout << "Copy Constructor Called\n";
+        a = obj.a;
+    }
+    void display() {
+        cout << "The value is : " << a << "\n";
+    }
+};
+int main() {
+  myclass x, y, z(45);
+  x.display();
+  y.display();
+  z.display();
+  myclass z1(z);
+  z1.display();
+  return 0;
+}
 
-// //In The Name of ALLAH
-// #include<bits/stdc++.h>
-// using namespace std;
-// class myclass {
-//    int a;
-// public:
-//     myclass() {
-//         a = 10;
-//     }
-//     myclass(int n) {
-//         a = n;
-//     }
-//     void display() {
-//         cout << "The value is : " << a << "\n";
-//     }
-// };
-// int main() {
-//   myclass x, y, z(45);
-//   x.display();
-//   y.display();
-//   z.display();
-//   return 0;
-// }
+//In The Name of ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+class myclass {
+   int a;
+public:
+    myclass() {
+        a = 10;
+    }
+    myclass(int n) {
+        a = n;
+    }
+    void display() {
+        cout << "The value is : " << a << "\n";
+    }
+};
+int main() {
+  myclass x, y, z(45);
+  x.display();
+  y.display();
+  z.display();
+  return 0;
+}
 
 //In The Name of ALLAH
 #include<bits/stdc++.h>
