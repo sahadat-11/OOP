ebdbcac8fb229528c587f400c82eb25315eae6e1
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-07-31 07:40:19 +0600

    changed

--- modes_of_inheritance.cpp
+++ modes_of_inheritance.cpp
@@ -1,32 +1,59 @@
-// C++ Implementation to show that a derived class
-// doesn’t inherit access to private data members.
-// However, it does inherit a full parent object.
-class A {
+// // C++ Implementation to show that a derived class
+// // doesn’t inherit access to private data members.
+// // However, it does inherit a full parent object.
+// class A {
+// public:
+// 	int x;
+
+// protected:
+// 	int y;
+
+// private:
+// 	int z;
+// };
+
+// class B : public A {
+// 	// x is public
+// 	// y is protected
+// 	// z is not accessible from B
+// };
+
+// class C : protected A {
+// 	// x is protected
+// 	// y is protected
+// 	// z is not accessible from C
+// };
+
+// class D : private A // 'private' is default for classes
+// {
+// 	// x is private
+// 	// y is private
+// 	// z is not accessible from D
+// };
+
+//In The Name of ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+class samp {
+	int i;
 public:
-	int x;
-
-protected:
-	int y;
-
-private:
-	int z;
+	samp(int n) {
+		i = n;
+		cout << "Constructing\n";
+	}
+	~samp() {
+		cout << "Destructing\n";
+	}
+	int get_a() {
+		return i;
+	}
 };
-
-class B : public A {
-	// x is public
-	// y is protected
-	// z is not accessible from B
-};
-
-class C : protected A {
-	// x is protected
-	// y is protected
-	// z is not accessible from C
-};
-
-class D : private A // 'private' is default for classes
-{
-	// x is private
-	// y is private
-	// z is not accessible from D
-};
+int square(samp o) {
+	return o.get_a() * o.get_a();
+}
+int main() {
+	samp a(10);
+	cout << square(a) << "\n";
+    
+  return 0;
+}
