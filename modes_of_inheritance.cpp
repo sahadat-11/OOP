a34c9f1b671a30bf71ca035d507345a6aa7325be
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-07-31 10:48:50 +0600

    changed

--- modes_of_inheritance.cpp
+++ modes_of_inheritance.cpp
@@ -1,60 +1,33 @@
-// // C++ Implementation to show that a derived class
-// // doesn’t inherit access to private data members.
-// // However, it does inherit a full parent object.
-// class A {
-// public:
-// 	int x;
+// C++ Implementation to show that a derived class
+// doesn’t inherit access to private data members.
+// However, it does inherit a full parent object.
+class A {
+public:
+	int x;
 
-// protected:
-// 	int y;
+protected:
+	int y;
 
-// private:
-// 	int z;
-// };
-
-// class B : public A {
-// 	// x is public
-// 	// y is protected
-// 	// z is not accessible from B
-// };
-
-// class C : protected A {
-// 	// x is protected
-// 	// y is protected
-// 	// z is not accessible from C
-// };
-
-// class D : private A // 'private' is default for classes
-// {
-// 	// x is private
-// 	// y is private
-// 	// z is not accessible from D
-// };
-
-// C++ program to implement
-// Hierarchical Inheritance
-#include <iostream>
-
-using namespace std;
-
-// base class
-class Vehicle {
-  public: Vehicle() {
-    cout << "This is a Vehicle\n";
-  }
+private:
+	int z;
 };
 
-// first sub class
-class Car: public Vehicle {};
+class B : public A {
+	// x is public
+	// y is protected
+	// z is not accessible from B
+};
 
-// second sub class
-class Bus: public Vehicle {};
+class C : protected A {
+	// x is protected
+	// y is protected
+	// z is not accessible from C
+};
 
-// main function
-int main() {
-  // Creating object of sub class will
-  // invoke the constructor of base class.
-  Car obj1;
-  //Bus obj2;
-  return 0;
-}
+class D : private A // 'private' is default for classes
+{
+	// x is private
+	// y is private
+	// z is not accessible from D
+};
+
